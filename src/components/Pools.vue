2ea0a9655e235526e06fd4f9d9789e8b6bfb4bbe
ghost0x <ghost0x7@pm.me>
2022-01-01 06:03:14 -0500

    Filter low fee/liquidity pools, updated UI

--- src/components/Pools.vue
+++ src/components/Pools.vue
@@ -2,24 +2,28 @@
   <div class="hello">
     <h2 class="text-2xl mb-4">Pools</h2>
   
-    <span class="text-lg">Found {{pools.length}} pools.</span>
+    <div v-if="pools.length > 0" class="text-lg">Found {{pools.length}} pools.</div>
+    <div v-if="pools.length == 0">Loading data, please wait...</div>
 
-  <table class="mt-4">
+    <div v-if="pools.length > 0" class="row-auto"><input type="checkbox" @change="refresh($event)" v-model="hideLowLiquidity" /> Hide pools with &lt; low liquidity</div>
+    <div v-if="pools.length > 0" class="row-auto"><input type="checkbox" @change="refresh($event)" v-model="hideLowFees" /> Hide pools with &lt; low fees</div>
+
+  <table class="mt-4" v-if="pools.length > 0">
     <thead>
       <tr>
         <th></th>
         <th>Pool</th>
         <th>TVL</th>
         <th>Fees</th>
-        <th>Fee:TVL Ratio</th>
+        <th>Fee/TVL Ratio</th>
       </tr>
     </thead>
       <tbody>
         <tr v-for="(pool, index) in pools" :key="pool.id">
           <td>{{index + 1}}</td>
           <td><a target="_blank" :href="'https://info.uniswap.org/#/pools/' + pool.pool.id">{{ pool.pool.token0.name }}/{{ pool.pool.token1.name }}</a></td>
-          <td>${{parseFloat(pool.feesUSD).toLocaleString()}}</td>
           <td>${{parseFloat(pool.tvlUSD).toLocaleString()}}</td>
+          <td>${{parseFloat(pool.feesUSD).toLocaleString()}}</td>
           <th>{{pool.feeTvlRatio}}</th>
         </tr>
     </tbody>
@@ -33,26 +37,39 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      pools: Array
+      pools: Array,
+      rawPoolData: Array,
+      hideLowLiquidity: true,
+      hideLowFees: true
     }
   },
-  mounted() {
-    this.pools = [];
-    var dateOffset = (24*60*60*1000) * 5; //5 days
-    var timestamp = new Date();
-    timestamp.setTime(timestamp.getTime() - dateOffset);
-    //timestamp = timestamp.getTime() / 1000;
-    timestamp = parseInt(timestamp.getTime() / 1000);
-    this.axios.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', 
-    { 
-      query: '{    poolDayDatas(first: 1000, orderBy: feesUSD,      orderDirection: desc, where:{        date_gt:' + timestamp + ' })  {      id,   pool {     id ,    token0 { name }   ,  token1 { name }   }  , tvlUSD  , feesUSD  } }' })
-    .then((response) => {
-      var pools = response.data.data.poolDayDatas;
-      for(let i = 0; i < pools.length; i++) {
-        pools[i].feeTvlRatio = (parseFloat(pools[i].feesUSD) / parseFloat(pools[i].tvlUSD)).toFixed(3);
+  methods: {
+    refresh() {
+      console.log("Refresh");
+      var pools = [];
+      for(let i = 0; i < this.rawPoolData.length; i++) {
+        var pool = this.rawPoolData[i];
+        if (this.hideLowLiquidity && pool.tvlUSD < 2000000) continue;
+        if (this.hideLowFees && pool.feesUSD < 5000) continue;
+        pool.feeTvlRatio = ((parseFloat(pool.feesUSD) / parseFloat(pool.tvlUSD)) * 100).toFixed(4);
+        pools.push(pool);
       }
       pools.sort((a, b) => (a.feeTvlRatio < b.feeTvlRatio) ? 1 : -1)
       this.pools = pools;
+    },
+  },
+  mounted() {
+    this.pools = [];
+    var dateOffset = (24*60*60*1000) * 1; //1 days
+    var timestamp = new Date();
+    timestamp.setTime(timestamp.getTime() - dateOffset);
+    timestamp = parseInt(timestamp.getTime() / 1000);
+    this.axios.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', 
+    { 
+      query: '{    poolDayDatas(first: 1000, orderBy: feesUSD, orderDirection: desc, where:{ date_gt:' + timestamp + ' })  { id,  pool {     id ,   feesUSD, token0 { name }   ,  token1 { name }   }  , tvlUSD  , feesUSD  } }' })
+    .then((response) => {
+      this.rawPoolData = response.data.data.poolDayDatas;
+      this.refresh();
     })
   }
 }
